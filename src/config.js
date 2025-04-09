d0b7d3ab9bf271644c56ba1610bf114dfbaa6f3c
DannielMurcia <71535217+murciaue4@users.noreply.github.com>
2025-04-09 01:23:01 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -18,11 +18,11 @@ if (process.env.DB_RAILWAY) {
   console.warn('No se encontró la variable DB_RAILWAY, utilizando configuración local.');
 
   dbConfig = {
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '72420',
-    database: process.env.DB_NAME || 'railway',
-    port: process.env.DB_PORT || 3306,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER, 
+    password: process.env.DB_PASSWORD, 
+    database: process.env.DB_NAME, 
+    port: process.env.DB_PORT 
   };
 }
 
